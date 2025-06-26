b58df8db9fcf0b9d4daefac5e32a24614f9064d3
hadipirhadi2025 <hadi.pirhadi2025@gmail.com>
2025-06-26 11:13:46 +0200

    Add KlickHandler class to implement ActionListener for button click events

---
+++ src/de/bunt/alles/KlickHandler.java
@@ -0,0 +1,29 @@
+package de.bunt.alles;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+/**
+ *     1) Eigene Klasse schreiben, die das ActionListener Interface implementiert
+ *     2) Legt fest, was beim Aufruf (durch JButton) der actionPerformed Metode passieren soll
+ *     3) Erstellen eines Objekts dieser Klasse
+ *     4) Objekt beim JButton registrieren (addActionListener)
+ *     5) testen
+ *
+ *     Ein Klickhandler ist ein ActcíonListener (imlements)
+ *     ActionListener actionListenerReferenz = new KlickHandler();
+ *
+ */
+public class KlickHandler implements ActionListener {
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        LocalTime jetzt = LocalTime.now();
+        DateTimeFormatter formatHelfer = DateTimeFormatter.ofPattern("HH:mm:ss");
+        String jetztAlsString = jetzt.format(formatHelfer);
+        System.out.println(jetztAlsString);
+
+    }
+}
