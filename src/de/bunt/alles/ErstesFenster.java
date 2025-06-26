31500f571653297b83b3d54a2b0e575856adcf95
hadipirhadi2025 <hadi.pirhadi2025@gmail.com>
2025-06-26 09:33:36 +0200

    Add ErstesFenster class to demonstrate user interaction with GUI components

---
+++ src/de/bunt/alles/ErstesFenster.java
@@ -0,0 +1,15 @@
+package de.bunt.alles;
+
+/**
+ * <pre>
+ *     Demonstriert wie Fenster (für Benutzerinteraktion) erstellt werden
+ *     Zeigt, wie Benutzerinteraktionen entgegen genommen werden.
+ *
+ *     Verwendet werden hier Klassen, die für Benutzerinteraktion vorgehen sind, Fenster, Benutzer, Textfelder ...
+ *
+ *     Klassen aus dem Package javax.swing: Graphische Komponenten
+ *                     Package java.awt: Ereignisbehandlung, Schriften, Farben, Organisation
+ * </pre>
+ */
+public class ErstesFenster {
+}
