5d82ab0db3153260400dfc5a777ca01bd89c073e
hadipirhadi2025 <hadi.pirhadi2025@gmail.com>
2025-06-26 12:25:51 +0200

    Add inner class InnerKlickHandler to handle button click events in ZweitesFenster

--- src/de/bunt/alles/ZweitesFenster.java
+++ src/de/bunt/alles/ZweitesFenster.java
@@ -2,8 +2,27 @@ package de.bunt.alles;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 
 public class ZweitesFenster extends JFrame {
+
+    /**
+     * Inner Klasse: Wird erstellt innerhalb einer "ormalen" Klasse: äußere Klasse
+     * Inner Klassen haben Zugriff auf Attribute und Methoden(Member) der äußeren Klasse
+     * Bei Event-Behandlung wird meist inneren Klassen gearbeitet
+     */
+    private class InnerKlickHandler implements ActionListener{
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            LocalTime jetzt = LocalTime.now();
+            DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+            String jetztAlsString = jetzt.format(dateTimeFormatter);
+            uhrzeitAnzeige.setText(jetztAlsString);
+        }
+    }
 
     private JButton klickMich = new JButton("Klick mich");
     private JLabel uhrzeitAnzeige = new JLabel("XX:XX:XX");
@@ -23,5 +42,11 @@ public class ZweitesFenster extends JFrame {
         add(platz);
         add(platz);
 
+        verwendenInnerKlickHandler();
+    }
+
+    private void verwendenInnerKlickHandler() {
+        InnerKlickHandler meinHandler = new InnerKlickHandler();
+        klickMich.addActionListener(meinHandler);
     }
 }
